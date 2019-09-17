2f1c82d504ef2d516b46cee93da49de97a831ecc
Jack Toumey <jack@toumey.io>
2019-09-16 23:30:47 -0700

    Adding note to self

--- config-overrides.js
+++ config-overrides.js
@@ -4,5 +4,8 @@ const { override, useEslintRc } = require('customize-cra');
 const path = require('path');
 
 module.exports = override(
+  // reminding myself because I might need this:
+  // addDecoratorsLegacy(),
+
   useEslintRc(path.resolve(__dirname, '.eslintrc'))
 );
